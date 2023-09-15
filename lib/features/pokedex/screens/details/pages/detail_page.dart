5c88d3ac0da3a564612c1ac472a460e69b209304
Scalco <fscalco7@gmail.com>
2023-09-15 10:05:20 -0300

    funciona

---
+++ lib/features/pokedex/screens/details/pages/detail_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_copy/common/models/pokemon.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key, required this.name, required this.list});
+  final String name;
+  final List<Pokemon> list;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          name,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 400,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 32),
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: list
+                      .map(
+                        (e) => Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Text(e.name),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
